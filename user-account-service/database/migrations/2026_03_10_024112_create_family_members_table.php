e1fdabae09303935530847777a4d228f6325d893
Andre <andresaragih932@gmail.com>
2026-03-28 15:10:44 +0700

    feat: setup skema database user dan family

--- user-account-service/database/migrations/2026_03_10_024112_create_family_members_table.php
+++ user-account-service/database/migrations/2026_03_10_024112_create_family_members_table.php
@@ -10,17 +10,18 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('family_members', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->string('full_name');
-            $table->string('relationship');
-            $table->string('gender');
-            $table->date('birth_date');
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('family_members', function (Blueprint $table) {
+        $table->id();
+        $table->uuid('user_id'); // ID dari user (Jemaat Aktif) yang memasukkan data keluarga ini
+        $table->string('full_name', 255);
+        $table->string('relationship', 50); // Contoh: 'Suami', 'Istri', 'Anak'
+        $table->string('gender', 20)->nullable();
+        $table->date('birth_date')->nullable();
+        $table->timestamps();
+        $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+    });
+}
 
     /**
      * Reverse the migrations.
